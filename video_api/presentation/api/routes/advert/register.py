57646fdafe3d4381a32a71325df8144d502b1468
likeinlife <likeinlife@outlook.com>
2024-06-06 23:47:42 +0300

    fix(presentation.api): advert and user route prefixes

--- video_api/presentation/api/routes/advert/register.py
+++ video_api/presentation/api/routes/advert/register.py
@@ -4,7 +4,7 @@ from . import endpoints
 
 
 def register(router: APIRouter) -> None:
-    _router = APIRouter(tags=["advert"])
+    _router = APIRouter(prefix="/advert", tags=["advert"])
     _router.include_router(endpoints.router)
 
     router.include_router(_router)
--- video_api/presentation/api/routes/user/register.py
+++ video_api/presentation/api/routes/user/register.py
@@ -4,7 +4,7 @@ from . import endpoints
 
 
 def register(router: APIRouter) -> None:
-    _router = APIRouter(tags=["user"])
+    _router = APIRouter(prefix="/user", tags=["user"])
     _router.include_router(endpoints.router)
 
     router.include_router(_router)
